25627e21f08b4789e5cfca649c22d11acae39c0a
rickguo2011 <rickguo2011@gmail.com>
2017-03-01 15:30:47 -0800

    Update Q279.java

--- Q279.java
+++ Q279.java
@@ -15,6 +15,22 @@ public class Solution {
    //                                                     dp[n - 9], //9 = 3^2
    //                                                        ...
    //                                                   );
+    
+    public class Solution {
+    public int numSquares(int n) {
+        if (n < 1) return 0;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++){
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
+    
+    
    int[] dp = new int[n + 1];
    for (int i = 1; i <= n; i++){
       dp[i] = Integer.MAX_VALUE;
