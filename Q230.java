6f1b64b1fa9ada2e30fcc68f51f735af26855fa6
rickguo2011 <rickguo2011@gmail.com>
2017-03-02 07:25:08 -0800

    Update Q230.java

--- Q230.java
+++ Q230.java
@@ -23,12 +23,41 @@ Hint:
  *     TreeNode(int x) { val = x; }
  * }
  */
+        //for follow up
+        search(root, k){
+        k--;
+        if (k == 0){
+            while(root.left != null){
+                root = root.left;
+            }
+            return root;
+        }
+        if (root.leftNum > k){
+            search(root.left, k);
+        }
+        else if (root.leftNum < k){
+            search(root.right, k - root.leftNum - 1);
+        }
+        else{
+            return root;
+        }
+    
+    //for follow up insert:
+        insert(TreeNode parents(initially is just root, ok if k valid), TreeNode root, boolean leftN, boolean righN, int val){
+            if (root == null){
+                if (leftN) parents.left = new TreeNode(val);
+                if (rightN) parents.right = new TreeNode(val);
+                return;
+            }
+            if (root.val > val) insert(root, root.left, val, true, false);
+            if (root.val < val) insert(root, root.right, val, false, true);
+        }
 public class Solution {
     //this is trivial solution O(n)
     //for follow up, it is similar to cc150 chapter 11 Q
     //change the treeNode structure, to add the leftNum value as part of structure, which keeps the num of left childs when insert/delete
-    //then when searching for kth smallest, just first compare it with the root, if root.leftNum < k, then go right, if root.leftNum + righ.
-    //leftNum > k; then go left, if root.leftNum + right.left.leftNum == k, stop and return;
+    //then when searching for kth smallest, just first k-- and compare it with the root, if root.leftNum < k, then go right, if root.leftNum > k; then go left, if root.leftNum == k, stop and return root;
+    //see above code.
     public int kthSmallest(TreeNode root, int k) {
         ArrayList<Integer> tmp = new ArrayList<Integer>();
         kthRecursion(root, tmp);
