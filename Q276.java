6acde04d3fa1fd958cad2d485cab67b98c1f88b0
rickguo2011 <rickguo2011@gmail.com>
2017-03-03 09:25:45 -0800

    Update Q276.java

--- Q276.java
+++ Q276.java
@@ -8,6 +8,47 @@ Return the total number of ways you can paint the fence.
 
 Note:
 n and k are non-negative integers. 
+ 
+ public int numWays(int k, int n){
+    int[] dp = {0, k, k*k, 0};
+    if (n <= 2) return dp[n];
+    for (int i = 3; i <= n; i++){
+       dp[3] = dp[2] * (k - 1) + dp[1] * (k - 1);
+       dp[1] = dp[2];
+       dp[2] = dp[3];
+    }
+    return dp[3];
+ }思路是这样：
+ 
+ 第一个有K种，第二完后有K*K种，第三个：
+ K - 1 - (k - 1)    i.e. dp[1] *（ｋ　－　１）; 表示第二个和第一个一样颜色，第三个不同的一共几种
+ ｋ　－　(ｋ－１)　－　１　　　ｉ．ｅ．　k *（ｋ－１）　　＊1  表示第二个和第一个不同， 但第三个和第二个相同。
+ k - (k -1)  - (k - 1) i.e. ｋ　＊　(k - 1) * (k - 1) 表示第二个和第一个不同，但第三个可以和第一个相同，但不和第二个相同
+ 
+ 众合一下，k *（ｋ　－　１）　＊　１　＋k*(k-1)*(k-1) = k*k*(k-1) = dp[2] * (k-1)
+ 
+ 
+ FROM ON LINE: 这种给定一个规则，计算有多少种结果的题目一般都是动态规划，因为我们可以从这个规则中得到递推式。根据题意，不能有超过连续两根柱子是一个颜色，也就意味着第三根柱子要么根第一个柱子不是一个颜色，要么跟第二根柱子不是一个颜色。如果不是同一个颜色，计算可能性的时候就要去掉之前的颜色，也就是k-1种可能性。假设dp[1]是第一根柱子及之前涂色的可能性数量，dp[2]是第二根柱子及之前涂色的可能性数量，则dp[3]=(k-1)*dp[1] + (k-1)*dp[2]。
+
+递推式有了，下面再讨论下base情况，所有柱子中第一根涂色的方式有k中，第二根涂色的方式则是k*k，因为第二根柱子可以和第一根一样。
+ 
+ public class Solution {
+    public int numWays(int n, int k) {
+        // 当n=0时返回0
+        int dp[] = {0, k , k*k, 0};
+        if(n <= 2){
+            return dp[n];
+        }
+        for(int i = 2; i < n; i++){
+            // 递推式：第三根柱子要么根第一个柱子不是一个颜色，要么跟第二根柱子不是一个颜色
+            dp[3] = (k - 1) * (dp[1] + dp[2]);
+            dp[1] = dp[2];
+            dp[2] = dp[3];
+        }
+        return dp[3];
+    }
+}
+ 
 
 public class Solution {
     
