81f8ebf1b5a986fceb04e1fc933ae7b697743375
rickguo2011 <rickguo2011@gmail.com>
2017-03-06 19:11:01 -0800

    Update Guess Number Higher or Lower II375.java

--- Guess Number Higher or Lower II375.java
+++ Guess Number Higher or Lower II375.java
@@ -18,10 +18,7 @@ Game over. 8 is the number I picked.
 
 You end up paying $5 + $7 + $9 = $21.
 
-Given a particular n ≥ 1, find out how much money you need to have to guarantee a win.
-
-
-
+Given a particular n ≥ 1, find out how much money you need to have to guarantee a win. (这里问的是最小需要的钱数能GUARANTEE)
 我们假定选择了一个错误的数x，（1<=x<=n && x!=y ）那么就知道接下来应该从[1,x-1 ] 或者[x+1,n]中进行查找。 假如我们已经解决了[1,x-1] 和 [x+1,n]计算问题，我们将其表示为solve(L,x-1) 和solve(x+1,n)，那么我们应该选择max(solve(L,x-1),solve(x+1,n)) 这样就是求最坏情况下的损失。总的损失就是 f(x) = x + max(solve(L,x-1),solve(x+1,n))
 
 那么将x从1~n进行遍历，取使得 f(x) 达到最小，来确定最坏情况下最小的损失，也就是我们初始应该选择哪个数。
