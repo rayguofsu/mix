650395f5473fff4af274abaea3110d4d6f2bb45b
rickguo2011 <rickguo2011@gmail.com>
2017-03-08 09:32:44 -0800

    Update Q240.java

--- Q240.java
+++ Q240.java
@@ -21,6 +21,38 @@ Consider the following matrix:
 Given target = 5, return true.
 
 Given target = 20, return false.
+        // check corner case
+      public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return 0;
+        }
+        
+        // from bottom left to top right
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int x = n - 1;
+        int y = 0;
+        int count = 0;
+        
+        while (x >= 0 && y < m) {
+            if (matrix[x][y] < target) {
+                y++;
+            } else if (matrix[x][y] > target) {
+                x--;
+            } else {
+                count++;
+                x--;
+                y++;
+            }
+        }
+        return count;
+    }
+
+
+
 
 public class Solution {
     //idea is mine: just make use of the property to reduce the columns and rows via binary search
