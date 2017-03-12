67f54309693ef9e5596befbae4a0971383cf8cf0
rickguo2011 <rickguo2011@gmail.com>
2017-03-12 07:58:42 -0700

    Update Q146.java

--- Q146.java
+++ Q146.java
@@ -11,7 +11,76 @@ Used an ArrayList to maintain the least visited/set node. If the node was set be
 Used a HashMap to maintain the cache table. If there is no space left, find the least used node (which should be the first node in the LinkedList, delete it in both the HashMap and ArrayList, and add the new one in both places.)
 
 One of the trick thing is that -- if there are no spaces left (when left > 0 is false), removing the least used one and adding the new one, but this operation does not need to increment left anymore (it's just a replacement, and left will be kept at 0)!
-public class LRUCache {
+public class LRUCache {//O(1)
+    private class Node{
+        Node prev;
+        Node next;
+        int key;
+        int value;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        tail.prev = head;
+        head.next = tail;
+    }
+
+    public int get(int key) {
+        if( !hs.containsKey(key)) {
+            return -1;
+        }
+
+        // remove current
+        Node current = hs.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        // move current to tail
+        move_to_tail(current);
+
+        return hs.get(key).value;
+    }
+
+    public void set(int key, int value) {
+        if( get(key) != -1) {
+            hs.get(key).value = value;
+            return;
+        }
+
+        if (hs.size() == capacity) {
+            hs.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node insert = new Node(key, value);
+        hs.put(key, insert);
+        move_to_tail(insert);
+    }
+
+    private void move_to_tail(Node current) {
+        current.prev = tail.prev;
+        tail.prev = current;
+        current.prev.next = current;
+        current.next = tail;
+    }
+}
+
+
+
+public class LRUCache {//O(N)
     private int cap;
     ArrayList<Integer> list;
     Map<Integer, Integer> map;
