a127ca99d0d3a189e9b4e775a8ff75f0e02adce1
rickguo2011 <rickguo2011@gmail.com>
2017-03-14 09:11:05 -0700

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -32,3 +32,25 @@ public class Solution {
         return node;
     }
 }
+public class Solution {//for add number II can use below also, but use two stack for storeing l1 and l2 first; see recurstion vs stack
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode tmp = new ListNode(0);
+        ListNode headCp = tmp;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            int a1 = l1 == null ? 0 : l1.val;
+            int a2 = l2 == null ? 0 : l2.val;
+            int sum = a1 + a2 + carry;
+            carry = sum / 10;
+            sum %= 10;
+            tmp.next = new ListNode(sum);
+            tmp = tmp.next;
+            l1 = l1 == null ? l1 : l1.next;
+            l2 = l2 == null ? l2 : l2.next;
+        }
+        return headCp.next;
+        
+    }
+}
