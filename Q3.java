c69363a337eeb8c6123a447f952f54381b693d4a
rickguo2011 <rickguo2011@gmail.com>
2017-03-18 18:24:00 -0700

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -9,7 +9,33 @@ Given "abcabcbb", the answer is "abc", which the length is 3.
 Given "bbbbb", the answer is "b", with the length of 1.
 
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int start = 0, i = 0;
+        Set<Character> set = new HashSet<>();
+        int max = 0;
+        while(i < s.length()){
+            char c = s.charAt(i);
+            if (!set.contains(c)){
+                set.add(c);
+                max = Math.max(max, set.size());
+            }
+            else{
+                while(set.contains(c)){
+                    set.remove(s.charAt(start++));
+                }
+                set.add(c);
+            }
+            i++;
+        }
+        return max;
+    }
+    
+}
+    
+    
+    
 public class Solution { //My solution is much eaiser to understand.
     public int lengthOfLongestSubstring(String s){
         int max = 0;
