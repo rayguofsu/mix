69c46628edc0cabca6bff37e4ec6566f3bcfe2c3
rickguo2011 <rickguo2011@gmail.com>
2017-03-24 17:42:08 -0700

    Update Q347.java

--- Q347.java
+++ Q347.java
@@ -9,6 +9,40 @@ Note:
 
     You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
     Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+     public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {//I prefer this one. O(N*logK)
+        //Remember using PriorityQueue
+        //Remember map.entrySet()
+        PriorityQueue<Map.Entry<Integer, Integer>> que = new PriorityQueue<Map.Entry<Integer, Integer>>(new Comparator<Map.Entry<Integer, Integer>>(){
+            public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2){
+                return e1.getValue() - e2.getValue();
+            }
+        });
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()){
+            if (que.size() < k){
+                que.offer(entry);
+            }
+            else{
+                if (que.peek().getValue() < entry.getValue()){
+                    que.poll();
+                    que.offer(entry);
+                }
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        while (!que.isEmpty()){
+            list.add(que.poll().getKey());
+        }
+        return list;
+    }
+}
+     
+     
 O(n) Solution - Bucket Sort
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
