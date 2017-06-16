e5bf51c36320a4b5e2014c28dcfb08d15db6e5e6
rayguofsu <raymondguofsu@gmail.com>
2017-06-16 08:45:40 -0700

    Update Q61.java

--- Q61.java
+++ Q61.java
@@ -23,6 +23,43 @@ return 4->5->1->2->3->NULL.
  *     ListNode(int x) { val = x; }
  * }
  */
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        k %= length(head);
+        while(fast != null && k > 0){
+            fast = fast.next;
+            k--;
+        }
+        if (fast == null) return head;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        fast.next = head;
+        head = slow.next;
+        slow.next = null;
+        return head;
+    }
+    private int length(ListNode head){
+        int l = 0;
+        while(head != null){
+            head = head.next;
+            l++;
+        }
+        return l;
+    }
+}
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
