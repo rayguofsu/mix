66ab30fb498987851e8c34818c2753040af14c27
rayguofsu <raymondguofsu@gmail.com>
2017-06-20 08:45:24 -0700

    Update Q88.java

--- Q88.java
+++ Q88.java
@@ -2,6 +2,15 @@
 //
 //Note:
 //You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int i = m + n - 1;
+        while(p1 >= 0 && p2 >= 0){
+            if (nums1[p1] < nums2[p2]) nums1[i--] = nums2[p2--];
+            else nums1[i--] = nums1[p1--];
+        }
+        while(p2 >= 0) nums1[i--] = nums2[p2--];
+    }
 
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
