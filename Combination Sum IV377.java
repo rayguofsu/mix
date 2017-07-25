2c784bbc1716ac7958ffe85bb193433623517119
rayguofsu <raymondguofsu@gmail.com>
2017-07-25 16:54:35 -0700

    Update Combination Sum IV377.java

--- Combination Sum IV377.java
+++ Combination Sum IV377.java
@@ -22,21 +22,39 @@ Follow up:
 What if negative numbers are allowed in the given array?
 How does it change the problem?
 What limitation we need to add to the question to allow negative numbers? 
-
-
-public class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
-        Arrays.sort(nums);
         dp[0] = 1;
-        for (int i = 1; i <= target; i++){
-            dp[i] = 0;
-            for (int j = 0 ; j < nums.length; j++){
-                if (i >= nums[j]){
-                    dp[i] += dp[i - nums[j]];
-                }
+        for (int i = 0; i <= target; i++){
+            for (int j = 0; j < nums.length; j++){
+                if (i >= nums[j]) dp[i] += dp[i - nums[j]];
             }
         }
         return dp[target];
+        
+        
     }
-}
+    
+    
+    
+    
+    
+   /* private int globalCnt = 0;
+    public int combinationSum4(int[] nums, int target) {//slow O(N!)
+        Arrays.sort(nums);
+        helper(nums, target, 0);
+        return globalCnt;
+    }
+    private void helper(int[] nums, int target, int sum){
+        if (sum == target){
+            globalCnt++;
+            return;
+        }
+        for (int i = 0; i < nums.length; i++){
+            if (target < sum + nums[i]) break;
+            helper(nums, target, sum + nums[i]);
+        }
+    
+    }
+    */
+
